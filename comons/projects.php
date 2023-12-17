231b7728e0e75c31bd3ce4f341d5e9360cf6135f
Fernando Perez <fernandohps17@gmail.com>
2023-12-16 22:50:44 -0400

    Se finalizo la section de projects para todas las pantallas

--- comons/projects.php
+++ comons/projects.php
@@ -8,6 +8,69 @@
                 <h2>La calidad es nuestra prioridad</h2>
             </section>
 
+            <div class="projects_gallery">
+
+                <picture class="picture-defer img-1">
+                    <source class="image source" type='image/webp' data-img="/public/img/textile-gallery-1.webp">
+                    <img class="image" data-img="/public/img/textile-gallery-1.png" alt="Textile imagen galelia" title="Textile imagen galeria" width="100" height="100">
+                </picture>
+
+                <picture class="picture-defer">
+                    <source class="image source" type='image/webp' data-img="/public/img/textile-gallery-2.webp">
+                    <img class="image" data-img="/public/img/textile-gallery-2.jpg" alt="Textile imagen galelia" title="Textile imagen galeria" width="100" height="100">
+                </picture>
+
+                <picture class="picture-defer">
+                    <source class="image source" type='image/webp' data-img="/public/img/textile-gallery-3.webp">
+                    <img class="image" data-img="/public/img/textile-gallery-3.jpg" alt="Textile imagen galelia" title="Textile imagen galeria" width="100" height="100">
+                </picture>
+
+                <picture class="picture-defer img-2">
+                    <source class="image source" type='image/webp' data-img="/public/img/textile-gallery-4.webp">
+                    <img class="image" data-img="/public/img/textile-gallery-4.jpg" alt="Textile imagen galelia" title="Textile imagen galeria" width="100" height="100">
+                </picture>
+
+                <picture class="picture-defer img-2">
+                    <source class="image source" type='image/webp' data-img="/public/img/textile-gallery-5.webp">
+                    <img class="image" data-img="/public/img/textile-gallery-5.jpg" alt="Textile imagen galelia" title="Textile imagen galeria" width="100" height="100">
+                </picture>
+
+            </div><br>
+
+
+
+            <section class="projects_title">
+                <h2>Nuestros patrocinantes</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus <br> nec ullamcorper mattis, pulvinar dapibus leo.</p>
+            </section>
+
+
+
+            <div class="projects_patrocinante">
+                <picture class="picture-defer">
+                    <source class="image source" type='image/webp' data-img="/public/img/textile-patrocinante-1.webp">
+                    <img class="image" data-img="/public/img/textile-patrocinante-1.png" alt="Textile imagen galelia" title="Textile imagen galeria" width="100" height="100">
+                </picture>
+
+                <picture class="picture-defer">
+                    <source class="image source" type='image/webp' data-img="/public/img/textile-patrocinante-2.webp">
+                    <img class="image" data-img="/public/img/textile-patrocinante-2.png" alt="Textile imagen galelia" title="Textile imagen galeria" width="100" height="100">
+                </picture>
+
+                <picture class="picture-defer">
+                    <source class="image source" type='image/webp' data-img="/public/img/textile-patrocinante-3.webp">
+                    <img class="image" data-img="/public/img/textile-patrocinante-3.png" alt="Textile imagen galelia" title="Textile imagen galeria" width="100" height="100">
+                </picture>
+
+                <picture class="picture-defer">
+                    <source class="image source" type='image/webp' data-img="/public/img/textile-patrocinante-4.webp">
+                    <img class="image" data-img="/public/img/textile-patrocinante-4.png" alt="Textile imagen galelia" title="Textile imagen galeria" width="100" height="100">
+                </picture>
+            </div><br>
+
+
+
+
         </div>
 
     </div>
