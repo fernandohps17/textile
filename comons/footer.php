d2da4100ed326f306a930f6c358c6df06f8964c8
Fernando Perez <fernandohps17@gmail.com>
2023-12-19 23:06:38 -0400

    Se creo el html de la section del footer

--- comons/footer.php
+++ comons/footer.php
@@ -2,13 +2,81 @@
     <div class="content">
 
         <div class="footer">
-            
-            <div class="footer_top"></div>
+
+            <div class="footer_top">
+                <section class="footer_top_text">
+                    <header>
+                        <h2>Nuestros boletines</h2>
+                    </header>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. <br> Ut elit tellus, luctus nec ullamcorper.</p>
+                </section>
+                <div class="footer_top_inputs">
+                    <input type="email" required placeholder="Email">
+                    <button>Enviar</button>
+                </div>
+            </div>
 
 
-            <div class="footer_bottom"></div>
+            <div class="footer_bottom">
 
+                <div class="footer_bottom_logo">
+                    <picture class="picture-defer">
+                        <source class="image source" type='image/webp' data-img="/public/img/textile-logo-footer.webp">
+                        <img class="image" data-img="/public/img/textile-logo-footer.png" alt="Textile logo footer" title="Textile logo footer" width="100" height="100">
+                    </picture>
+
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.</p>
+
+                    <div class="footer_bottom_icon">
+                        <a href="www.facebook.com"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                                <path d="M80 299.3V512H196V299.3h86.5l18-97.8H196V166.9c0-51.7 20.3-71.5 72.7-71.5c16.3 0 29.4 .4 37 1.2V7.9C291.4 4 256.4 0 236.2 0C129.3 0 80 50.5 80 159.4v42.1H14v97.8H80z" />
+                            </svg></a>
+                        <a href="www.twitter.com"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                                <path d="M459.4 151.7c.3 4.5 .3 9.1 .3 13.6 0 138.7-105.6 298.6-298.6 298.6-59.5 0-114.7-17.2-161.1-47.1 8.4 1 16.6 1.3 25.3 1.3 49.1 0 94.2-16.6 130.3-44.8-46.1-1-84.8-31.2-98.1-72.8 6.5 1 13 1.6 19.8 1.6 9.4 0 18.8-1.3 27.6-3.6-48.1-9.7-84.1-52-84.1-103v-1.3c14 7.8 30.2 12.7 47.4 13.3-28.3-18.8-46.8-51-46.8-87.4 0-19.5 5.2-37.4 14.3-53 51.7 63.7 129.3 105.3 216.4 109.8-1.6-7.8-2.6-15.9-2.6-24 0-57.8 46.8-104.9 104.9-104.9 30.2 0 57.5 12.7 76.7 33.1 23.7-4.5 46.5-13.3 66.6-25.3-7.8 24.4-24.4 44.8-46.1 57.8 21.1-2.3 41.6-8.1 60.4-16.2-14.3 20.8-32.2 39.3-52.6 54.3z" />
+                            </svg></a>
+                        <a href="www.linkeding.com"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                                <path d="M416 32H31.9C14.3 32 0 46.5 0 64.3v383.4C0 465.5 14.3 480 31.9 480H416c17.6 0 32-14.5 32-32.3V64.3c0-17.8-14.4-32.3-32-32.3zM135.4 416H69V202.2h66.5V416zm-33.2-243c-21.3 0-38.5-17.3-38.5-38.5S80.9 96 102.2 96c21.2 0 38.5 17.3 38.5 38.5 0 21.3-17.2 38.5-38.5 38.5zm282.1 243h-66.4V312c0-24.8-.5-56.7-34.5-56.7-34.6 0-39.9 27-39.9 54.9V416h-66.4V202.2h63.7v29.2h.9c8.9-16.8 30.6-34.5 62.9-34.5 67.2 0 79.7 44.3 79.7 101.9V416z" />
+                            </svg></a>
+                    </div>
+                </div>
+
+
+
+
+                <div class="footer_bottom_nav">
+                    <h2>Navegación</h2><br>
+
+                    <a href="/" class="<?php echo $inicio ?>">Inicio</a>
+                    <a href="#" class="<?php echo $nosotros ?>">Sobre Nosotros</a>
+                    <a href="#" class="<?php echo $servicios ?>">Servicios</a>
+                    <a href="#" class="<?php echo $proyecto ?>">Proyectos</a>
+                    <a href="#" class="<?php echo $contacto ?>">Contacto</a>
+                </div>
+
+
+
+
+                <div class="footer_bottom_work">
+                    <h2>Horas laborales</h2>
+
+                    <div class="footer_bottom_work_hours">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960"><path d="m612-292 56-56-148-148v-184h-80v216l172 172ZM480-80q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-400Zm0 320q133 0 226.5-93.5T800-480q0-133-93.5-226.5T480-800q-133 0-226.5 93.5T160-480q0 133 93.5 226.5T480-160Z"/></svg>
+                        <p>7 AM - 5 PM, Mon - Sat</p>
+                        <p>Lorem ipsum dolor sit amet, <br> consectetur adipiscing elit.</p>
+                    </div>
+
+                    <div class="footer_bottom_work_btn">
+                        <a href="">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960"><path d="M798-120q-125 0-247-54.5T329-329Q229-429 174.5-551T120-798q0-18 12-30t30-12h162q14 0 25 9.5t13 22.5l26 140q2 16-1 27t-11 19l-97 98q20 37 47.5 71.5T387-386q31 31 65 57.5t72 48.5l94-94q9-9 23.5-13.5T670-390l138 28q14 4 23 14.5t9 23.5v162q0 18-12 30t-30 12ZM241-600l66-66-17-94h-89q5 41 14 81t26 79Zm358 358q39 17 79.5 27t81.5 13v-88l-94-19-67 67ZM241-600Zm358 358Z"/></svg>
+                        <p>Llamanos</p>
+                        </a>
+                    </div>
+                </div>
+
+
+
+
+            </div>
         </div>
-
     </div>
 </div>
--- comons/nav.php
+++ comons/nav.php
@@ -67,11 +67,8 @@ if (strpos($_SERVER['REQUEST_URI'], 'servicios')) {
             </div>
         </div>
 
-
-
         <!-- Seccion Header -->
         <div class="header">
-
             <div class="header_img">
                 <picture class="picture-defer">
                     <source class="image source" type='image/webp' data-img="/public/img/textile-header-imagen-principal.webp">
@@ -85,9 +82,7 @@ if (strpos($_SERVER['REQUEST_URI'], 'servicios')) {
 
                     <a href="www.linkeding.com"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M416 32H31.9C14.3 32 0 46.5 0 64.3v383.4C0 465.5 14.3 480 31.9 480H416c17.6 0 32-14.5 32-32.3V64.3c0-17.8-14.4-32.3-32-32.3zM135.4 416H69V202.2h66.5V416zm-33.2-243c-21.3 0-38.5-17.3-38.5-38.5S80.9 96 102.2 96c21.2 0 38.5 17.3 38.5 38.5 0 21.3-17.2 38.5-38.5 38.5zm282.1 243h-66.4V312c0-24.8-.5-56.7-34.5-56.7-34.6 0-39.9 27-39.9 54.9V416h-66.4V202.2h63.7v29.2h.9c8.9-16.8 30.6-34.5 62.9-34.5 67.2 0 79.7 44.3 79.7 101.9V416z"/></svg></a>
                 </div>
-
             </div>
-
 
             <section class="header_content">
                 <p class="encabezado"><b>Ganador de premios 2021</b></p>
@@ -100,10 +95,6 @@ if (strpos($_SERVER['REQUEST_URI'], 'servicios')) {
                     <a href="#">Aprende más</a>
                 </div>
             </section>
-
         </div>
-
-
-
     </div>
 </div>
